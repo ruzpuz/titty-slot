77e7f53ac29b1b1bcf72c45f000960ed88e0b87c
Vladimir Ružić <vladimir@ruzic.info>
2016-10-26 16:03:26 +0200

    front end mock

--- view/assets/js/application.js
+++ view/assets/js/application.js
@@ -1,8 +1,6 @@
 (function () {
     window.slotMachine = {};
-    var image1 = document.getElementById('image1'),
-        image2 = document.getElementById('image2'),
-        image3 = document.getElementById('image3');
+    var animationDuration = 600;
 
     function hasClass(el, className) {
         if (el.classList) {
@@ -25,31 +23,76 @@
             el.className = el.className.replace(reg, ' ');
         }
     }
-    function animate() {
+    function animateWheel(images) {
+        var animationActive = false,
+            stopAnimation = false
+        function finishAnimation() {
+
+            removeClass(images[0], 'animate');
+            removeClass(images[0], 'gone');
+
+            removeClass(images[1], 'animate');
+            removeClass(images[1], 'gone');
+
+            removeClass(images[2], 'animate');
+            removeClass(images[2], 'gone');
+
+            animationActive = false;
+
+        }
+        function resetCircle(element) {
+            removeClass(element, 'animate');
+            removeClass(element, 'gone');
+            setTimeout(animationCircle, 20, element);
+        }
         function animationCircle(element) {
-            function step1() {
-                addClass(element, 'animate');
-                addClass(element, 'positioned');
-                setTimeout(step2, 105)
+            if(stopAnimation) {
+                setTimeout(finishAnimation, animationDuration);
+                addClass(images[3], 'jackpot');
+                return;
             }
-            function step2() {
-                addClass(element, 'removed');
-
-                setTimeout(step3, 105)
-            }
-            function step3() {
-                removeClass(element, 'animate');
-                removeClass(element, 'removed');
-                removeClass(element, 'positioned');
-
-                setTimeout(step1, 105)
-            }
-            step1();
+            addClass(element, 'animate');
+            addClass(element, 'gone');
+            setTimeout(resetCircle, animationDuration, element);
         }
 
-        animationCircle(image1)
-    }
+        if(animationActive) {
+            return;
+        }
+        animationActive = true;
+        stopAnimation = false;
+        setTimeout(animationCircle, 0, images[0]);
+        setTimeout(animationCircle, animationDuration / 3, images[1]);
+        setTimeout(animationCircle, 2 * animationDuration / 3, images[2]);
+        setTimeout(function () { stopAnimation = true; }, 6 * animationDuration)
 
+
+    }
+    function animate() {
+        var wheels = [
+            [
+                document.getElementById('image1'),
+                document.getElementById('image2'),
+                document.getElementById('image3'),
+                document.getElementById('jackpot-image1')
+            ],[
+                document.getElementById('image4'),
+                document.getElementById('image5'),
+                document.getElementById('image6'),
+                document.getElementById('jackpot-image2')
+            ],[
+                document.getElementById('image7'),
+                document.getElementById('image8'),
+                document.getElementById('image9'),
+                document.getElementById('jackpot-image3')
+            ]
+        ];
+
+        setTimeout(animateWheel, 0, wheels[0]);
+        setTimeout(animateWheel, animationDuration / 3, wheels[1]);
+        setTimeout(animateWheel, 2 * animationDuration / 3, wheels[2]);
+
+    }
     window.slotMachine.animate = animate;
     window.slotMachine.addClass = addClass;
     window.slotMachine.removeClass = removeClass;
